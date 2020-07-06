f55d736ff033987530b2c1960d65cf422e7972cd
Johnny Phelan <phelanjo@oregonstate.edu>
2020-07-06 15:54:36 -0700

    Create waterfalls table in database

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+  user=os.getenv("POSTGRES_USER"),
+  pw=os.getenv("POSTGRES_PW"),
+  url=os.getenv("POSTGRES_URL"),
+  db=os.getenv("POSTGRES_DB")
+)
+
+print(DB_URL)
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def homepage():
-  return render_template('index.html')
+db = SQLAlchemy(app)
 
-@app.route('/add.html/')
-def add():
-  return render_template('add.html')
+class Waterfall(db.Model):
+  __tablename__ = 'waterfall'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(200))
+  height = db.Column(db.String(200))
+  latitude = db.Column(db.String(200))
+  longitude = db.Column(db.String(200))
 
-@app.route('/edit.html/')
-def delete():
-  return render_template('edit.html')
+db.create_all()
+
+# @app.route('/')
+
+# @app.route('/add')
+
+# @app.route('/edit')
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run()
